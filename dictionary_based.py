e09098253cb784dc17b2abe6eb38664a96ab0c41
Emanuele Muzio <emanuele.muzio@techedgegroup.com>
2023-08-03 03:21:45 +0200

    bwt and plots

--- dictionary_based.py
+++ dictionary_based.py
@@ -3,6 +3,25 @@
 # A paramter of the program should be W, the length of the search buffer.
 # Compare the number of triplets denoted by 𝑧 produced by LZ77(T) (or by LZss) and the
 # number of the equal-letter runs denoted by 𝑟 produced by the BWT(T).
+import random
+import string
+import math
+import matplotlib.pyplot as plt
+
+def BWT_encode(T):
+    permutations = []
+    for i in range(0, len(T)):
+        permutations.append(T[i:] + T[:i])
+    
+    sorted_permutations = sorted(permutations)
+    
+    I = sorted_permutations.index(T)
+    
+    word = ''
+    
+    for p in sorted_permutations:
+        word = word + p[-1]
+    return word, I    
 
 def LZ77_encode(T, w):
     encoding = []
@@ -18,7 +37,7 @@ def LZ77_step(search_buffer, look_ahead_buffer, w):
     longest_subs = []
     
     for x in range(1, len(look_ahead_buffer) + 1):
-        if look_ahead_buffer[:x] in search_buffer:
+        if look_ahead_buffer[:x] in search_buffer[-w:]:
             longest_subs.append(look_ahead_buffer[:x])
         else:
             break
@@ -89,14 +108,9 @@ def LZss_step(search_buffer, look_ahead_buffer, w):
         longest_seq = max(longest_subs, key=len)
         l = len(longest_seq)
         o = len((search_buffer.split(longest_seq))[-1]) + l
-        new_search_buffer = search_buffer + look_ahead_buffer[:l+1]
+        new_search_buffer = search_buffer + look_ahead_buffer[:l]
         
-        if l < len(look_ahead_buffer):
-            s = look_ahead_buffer[l]    
-        else: 
-            s = ''
-        
-        new_look_ahead_buffer = look_ahead_buffer[l+1:]
+        new_look_ahead_buffer = look_ahead_buffer[l:]
         
         pair = [o, l] 
                 
@@ -109,20 +123,46 @@ def LZss_decode(lzss_enc):
         if isinstance(y, str):
             decoding = decoding + y
         else:
-            decoding = decoding + decoding[-x : -x+y]
+            seq = decoding[-x : len(decoding) - x + y]
+            decoding = decoding + seq
     return decoding
     
-T = ['babbababbaabbaabaabaaa','aabbabab']
-w = len(T)
+def equal_letter_runs(T):
+    runs = []
+    for i in range(len(T) - 1):
+        cont = 1
+        add = False
+        for j in range(i + 1, len(T)):
+            if T[i] == T[j]:
+                add = True
+                cont = cont + 1
+            if T[i] != T[j]:
+                break
+        if add:
+            runs.append(T[i] * cont)    
+        i = j + 1  
+    return runs 
 
-lz77_enc = LZ77_encode(T[-1], w)
-lz77_dec = LZ77_decode(lz77_enc)
-lzss_enc = LZss_encode(T[-1], w)
-lzss_dec = LZss_decode(lzss_enc)
+w = random.randint(5, 10)
 
-print(f'Text to encode: {T}\n')
-print(f'Seach buffer dimension: {w}\n')
-print(f'LZ77 Encoded text: {lz77_enc}\n')
-print(f'LZ77 Decoded text: {lz77_dec}\n')
-print(f'LZssEncoded text: {lzss_enc}\n')
-print(f'LZss Decoded text: {lzss_dec}\n')
+z_lz77 = []
+z_lzss = []
+r = []
+
+N_range = range(5, 100)
+
+for x in N_range:
+    T = ''.join(random.choices(string.ascii_uppercase, k=x))
+    z_lz77.append(len(LZ77_encode(T, w)))
+    z_lzss.append(len(LZss_encode(T, w)))
+    r.append(len(equal_letter_runs(T)))
+
+plt.subplot(1, 1, 1)    
+plt.plot(N_range, z_lz77, label = "LZ77")
+plt.plot(N_range, z_lzss, label = "LZSS")
+plt.plot(N_range, r, label = "BWT")
+plt.legend()
+plt.title('LZ77, LZSS, BWT')
+plt.xlabel('N')
+plt.ylabel('Length')
+plt.show()
